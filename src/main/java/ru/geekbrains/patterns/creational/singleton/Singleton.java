2875500124b6f412650afe77731ea8078289b0f3
Meshcheriakov-Mikhail <sekesh@mail.ru>
2021-04-19 15:52:35 +0700

    lesson3

--- src/main/java/ru/geekbrains/patterns/creational/singleton/Singleton.java
+++ src/main/java/ru/geekbrains/patterns/creational/singleton/Singleton.java
@@ -1,9 +1,12 @@
 package ru.geekbrains.patterns.creational.singleton;
 
 public class Singleton {
-    private static final Singleton instance;
+    private static Singleton instance;
 
     public static Singleton getInstance() {
+        if (instance == null) {
+            instance = new Singleton();
+        }
         return instance;
     }
 }
